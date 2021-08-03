0ed504a0a559c23830e653c9ce46a9570737eb1a
Hussam Albulushi <eng.albulushi@gmail.com>
2021-08-03 10:47:35 +0300

    Comments feature: edited the comment model and reaplied migration

--- BlogApp/mysite/blog/models.py
+++ BlogApp/mysite/blog/models.py
@@ -53,7 +53,7 @@ class Post(models.Model):
                             self.publish.day,
                             self.slug])
 
-class Comments(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
